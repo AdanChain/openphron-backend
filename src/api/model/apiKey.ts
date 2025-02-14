682ae0980d322ddd4b6c17052429d98a2b57fdc4
neonflux-io <adriancole0327@gmail.com>
2025-02-14 18:23:07 +0900

    fix bugt

--- src/api/model/apiKey.ts
+++ src/api/model/apiKey.ts
@@ -1,26 +1,11 @@
 import { Schema, model } from 'mongoose';
 
-const ApiKeySchema = new Schema({
-    apiKey: { type: String, required: true },
-    status: { type: String, default: 'active' },
-    createdAt: { type: Date, default: Date.now },
-});
-
-const userIdSchema = new Schema({
-    userId: { type: String, required: true },
-    status: { type: String, default: 'active' },
-    createdAt: { type: Date, default: Date.now },
-});
-
 const UserApiKeysSchema = new Schema({
-    userIds: {
-        type: Map,
-        of: [ApiKeySchema],
-    },
-    allApiKeys: {
-        type: Map,
-        of: userIdSchema
-    }
+    userId: String,
+    name: String,
+    apiKey: String,
+    status: { type: String, default: 'active' },
+    createdAt: { type: Date, default: new Date() },
 });
 
 const ApiKeyModel = model('ApiKey', UserApiKeysSchema);
--- src/api/service/apiKey.ts
+++ src/api/service/apiKey.ts
@@ -5,20 +5,27 @@ const apiKeyService = {
     createApiKey: async (userId: string | undefined, hint?: string) => {
         if (!userId) return false;
         const apiKey = generateApiKey(hint);
+
         if (await apiKeyDA.apiKeyExists(apiKey)) {
             await apiKeyService.createApiKey(userId, hint);
         }
-        await apiKeyDA.createApiKey(userId, apiKey);
+        await apiKeyDA.create({ userId, name: hint, apiKey });
         return apiKey;
     },
     deleteApiKey: async (apiKey: string | undefined) => {
-        if(!apiKey) return false;
-        await apiKeyDA.deleteApiKey(apiKey);
+        const apiKeyExists = await apiKeyDA.apiKeyExists(apiKey);
+        if (!apiKeyExists) {
+            return false;
+        }
+        await apiKeyDA.delete({ apiKey });
         return true;
     },
     getApiKeys: async (userId: string | undefined) => {
-        const userApiKeys = await apiKeyDA.getApiKeysByUserId(userId);
-        return userApiKeys;
+        const apiKeys = await apiKeyDA.userIdExists(userId);
+        if (!apiKeys) {
+            return [];
+        }
+        return apiKeys;
     },
     availableApiKey: async (apiKey: string | undefined) => {
         const apiKeyExists = await apiKeyDA.apiKeyExists(apiKey);
